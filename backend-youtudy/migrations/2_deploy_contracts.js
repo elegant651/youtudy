fd430e8dde34eb47b4f95aaadf8edf00eec5320d
elegant651 <korea651@gmail.com>
2020-10-24 23:44:20 +0900

    Deploy to matic

--- backend-youtudy/migrations/2_deploy_contracts.js
+++ backend-youtudy/migrations/2_deploy_contracts.js
@@ -1,10 +1,12 @@
 const ConvertLib = artifacts.require("ConvertLib");
 const YouCoin = artifacts.require("YouCoin");
 const Badge = artifacts.require("Badge");
+const Youtudy = artifacts.require("Youtudy");
 
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
+  deployer.link(ConvertLib, YouCoin);
   deployer.deploy(YouCoin);
   deployer.deploy(Badge);
+  deployer.deploy(Youtudy);
 };
